bcfa14f8d419a16695c392e84e735bddb953585d
osiel jacobo <osiel781@gmail.com>
2019-01-31 13:53:28 -0600

    admin con la pagina principal eventos para mi

--- apps/smartlink/views.py
+++ apps/smartlink/views.py
@@ -2,8 +2,12 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 
 def home(request):
-    return render(request,'index.html')
+
+
+    eventos_smartlink= Eventos.objects.all()
+    context = {'eventos_smartlink':eventos_smartlink}
+    return render(request,'index.html',context)
